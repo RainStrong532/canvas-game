1c6cfc3b5cabf7f129f254946ed34772b26dfbe1
manh van vu <vuvanmanhttvtvp@gmail.com>
2020-05-17 17:05:49 +0700

    set up project

---
+++ app-demo/src/pages/HomePage.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+class HomePage extends React.Component{
+    render(){
+        return(
+            <div className='home_page d-flex justify-content-center mt-5'>
+                <div className='btn btn-primary'>This is home page</div>
+            </div>
+        )
+    }
+}
+export default HomePage;
